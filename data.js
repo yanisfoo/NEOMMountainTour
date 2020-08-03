2d3e54beed632e2f0ffd93dfb42a6f2160897439
yanisfoo <68383375+yanisfoo@users.noreply.github.com>
2020-08-03 19:35:29 +0300

    360_Virtual_Tour

---
+++ data.js
@@ -0,0 +1,409 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-in-the-hill",
+      "name": "IN THE HILL",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5832080857393471,
+          "pitch": 0.0890994379434602,
+          "rotation": 0,
+          "target": "5-vault_panorama-fn"
+        },
+        {
+          "yaw": -1.4306652542375105,
+          "pitch": 0.07088978757781206,
+          "rotation": 0,
+          "target": "6-vault_panorama_1-new"
+        },
+        {
+          "yaw": -1.4996451556080324,
+          "pitch": 0.07548800099455732,
+          "rotation": 0,
+          "target": "1-lava0017_pano-1_draft"
+        },
+        {
+          "yaw": -1.4242664404355914,
+          "pitch": -0.04546352028342682,
+          "rotation": 0,
+          "target": "2-observatory"
+        },
+        {
+          "yaw": -1.9879848722269866,
+          "pitch": 0.13655464079597834,
+          "rotation": 0,
+          "target": "3-onthelake_4"
+        },
+        {
+          "yaw": -0.8295542848937902,
+          "pitch": -0.09073141283139918,
+          "rotation": 0,
+          "target": "4-ski-slope_panorama"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-lava0017_pano-1_draft",
+      "name": "LAVA0017_Pano 1_Draft",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": -0.03719614315021147,
+        "pitch": -0.1057135623173906,
+        "fov": 1.6653716266521625
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1365379767262267,
+          "pitch": -0.2946649452471899,
+          "rotation": 0,
+          "target": "5-vault_panorama-fn"
+        },
+        {
+          "yaw": -0.06857997642479319,
+          "pitch": 0.3666931651781571,
+          "rotation": 0,
+          "target": "6-vault_panorama_1-new"
+        },
+        {
+          "yaw": -0.6553351496498685,
+          "pitch": -0.6383904898939239,
+          "rotation": 0,
+          "target": "0-in-the-hill"
+        },
+        {
+          "yaw": 0.013127369397468058,
+          "pitch": 0.02698213823165929,
+          "rotation": 0,
+          "target": "3-onthelake_4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-observatory",
+      "name": "OBSERVATORY",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9398385030732221,
+          "pitch": 0.5142089206506757,
+          "rotation": 0,
+          "target": "5-vault_panorama-fn"
+        },
+        {
+          "yaw": -0.7217420313090006,
+          "pitch": 0.4451191647718158,
+          "rotation": 0,
+          "target": "5-vault_panorama-fn"
+        },
+        {
+          "yaw": -0.4448209426088212,
+          "pitch": 0.3947447696604627,
+          "rotation": 0,
+          "target": "6-vault_panorama_1-new"
+        },
+        {
+          "yaw": -0.22853805201824073,
+          "pitch": 0.042778272058793476,
+          "rotation": 0,
+          "target": "0-in-the-hill"
+        },
+        {
+          "yaw": -0.6422198195842022,
+          "pitch": -0.024018445636404095,
+          "rotation": 0,
+          "target": "4-ski-slope_panorama"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-onthelake_4",
+      "name": "ONTHELAKE_4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2815664297856753,
+          "pitch": -0.32276432763322305,
+          "rotation": 0,
+          "target": "0-in-the-hill"
+        },
+        {
+          "yaw": 1.9707944261031418,
+          "pitch": -0.21930737005555834,
+          "rotation": 0,
+          "target": "5-vault_panorama-fn"
+        },
+        {
+          "yaw": 1.8659040613587692,
+          "pitch": -0.1851167995376386,
+          "rotation": 0,
+          "target": "1-lava0017_pano-1_draft"
+        },
+        {
+          "yaw": 2.0801341056627543,
+          "pitch": -0.2878696706181998,
+          "rotation": 0,
+          "target": "6-vault_panorama_1-new"
+        },
+        {
+          "yaw": -3.087878901366322,
+          "pitch": -0.467498862158644,
+          "rotation": 0,
+          "target": "4-ski-slope_panorama"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-ski-slope_panorama",
+      "name": "SKI SLOPE_PANORAMA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8811354700684593,
+          "pitch": 0.29098360098614684,
+          "rotation": 0,
+          "target": "0-in-the-hill"
+        },
+        {
+          "yaw": 0.7080775989973773,
+          "pitch": 0.24855737544735668,
+          "rotation": 0,
+          "target": "3-onthelake_4"
+        },
+        {
+          "yaw": 1.639475890692074,
+          "pitch": -0.0004069858128445958,
+          "rotation": 0,
+          "target": "2-observatory"
+        },
+        {
+          "yaw": 1.5359475010265946,
+          "pitch": 0.17174544001002445,
+          "rotation": 0,
+          "target": "5-vault_panorama-fn"
+        },
+        {
+          "yaw": 1.3950281228656518,
+          "pitch": 0.2002500211394569,
+          "rotation": 0,
+          "target": "6-vault_panorama_1-new"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-vault_panorama-fn",
+      "name": "VAULT_PANORAMA FN",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.07212772428872327,
+          "pitch": 0.6213620531426347,
+          "rotation": 0,
+          "target": "6-vault_panorama_1-new"
+        },
+        {
+          "yaw": -0.7941003088439533,
+          "pitch": -0.5548467549518925,
+          "rotation": 0,
+          "target": "3-onthelake_4"
+        },
+        {
+          "yaw": 0.011814401792390683,
+          "pitch": -0.045665126018857194,
+          "rotation": 0,
+          "target": "0-in-the-hill"
+        },
+        {
+          "yaw": 3.065307479746517,
+          "pitch": 0.31073550281430684,
+          "rotation": 0,
+          "target": "1-lava0017_pano-1_draft"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-vault_panorama_1-new",
+      "name": "Vault_Panorama_1 NEW",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.112157319955953,
+          "pitch": -0.3394126342534687,
+          "rotation": 0,
+          "target": "5-vault_panorama-fn"
+        },
+        {
+          "yaw": -3.08397570002613,
+          "pitch": -0.1982596891567585,
+          "rotation": 0,
+          "target": "1-lava0017_pano-1_draft"
+        },
+        {
+          "yaw": -0.030398953932902373,
+          "pitch": 0.025797879830198767,
+          "rotation": 0,
+          "target": "3-onthelake_4"
+        },
+        {
+          "yaw": -0.7819321775759072,
+          "pitch": -0.7112662904408946,
+          "rotation": 0,
+          "target": "0-in-the-hill"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
